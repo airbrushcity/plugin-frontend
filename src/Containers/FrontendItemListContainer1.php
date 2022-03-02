92250258e64ec97414ef45e0659e975a26dcebe6
airbrushcity <a.kolbfleisch@airbrush-city.de>
2022-03-02 12:04:18 +0100

    update-to-theme

---
+++ src/Containers/FrontendItemListContainer1.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Frontend\Containers;
+
+use Ceres\Config\CeresConfig;
+use Plenty\Modules\Webshop\ItemSearch\SearchPresets\TagItems;
+use Plenty\Modules\Webshop\ItemSearch\Services\ItemSearchService;
+use Plenty\Plugin\Templates\Twig;
+
+/**
+ * Class FrontendItemListContainer1
+ *
+ * This class is a DataProvider. It is used to display a configurable list of items in your online store.
+ * @package Frontend\Containers
+ */
+class FrontendItemListContainer1
+{
+    /**
+     * Renders the template.
+     * @param Twig $twig The twig instance
+     * @param array $arg A list of items
+     * @return string
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function call(Twig $twig, $arg):string
+    {
+        return $twig->render('Frontend::Containers.ItemLists.ItemList1', ["item" => $arg[0]]);
+    }
+}
