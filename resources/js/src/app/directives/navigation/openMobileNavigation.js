fc3373ed72d5ba523a5a1b45067c65aba2ecd825
airbrushcity <a.kolbfleisch@airbrush-city.de>
2022-03-14 12:35:57 +0100

    del js

--- resources/js/src/app/directives/navigation/openMobileNavigation.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-
-Vue.directive("open-mobile-navigation",
-    {
-        bind(el)
-        {
-            el.onclick = () => ceresStore.commit("setIsMobileNavigationOpen", true);
-        }
-    });
